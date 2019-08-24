562360c59929c306f425c90cd8877e9a143ad0b0
ABHISHEK <abhishek.yadav_cs18@gla.ac.in>
2019-08-24 17:04:23 +0530

    variables are declared and are initialised

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -4,8 +4,8 @@
  * */
 package main;
 
-// import java.util.Scanner;
-// uncomment the above line to use input in your program.
+import java.util.Scanner;
+
 
 public class Gravity {
 	// TODO 1: Create the main method
@@ -15,6 +15,13 @@ public class Gravity {
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
 		 *   c. Calculate the result and print it to the screen.
 		 * */
+
+        float time = 0, distance = 0, speed = 0;
+        double gravity = 9.8;
+        Scanner scanner = new Scanner(System.in);
+        time = scanner.nextInt();
+
+
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
